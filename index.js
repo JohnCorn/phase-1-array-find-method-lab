f5e02212bbc5b6e99d6383db5d178ffee89f7ce7
John Corn <jcornacchioli@gmail.com>
2023-06-03 19:04:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(objArray)
+  {
+
+    for (let i = 0; i < objArray.length; i++)
+    {
+        if (objArray[i].result == "W"){
+            return objArray[i].year;
+        }
+    }
+  }
